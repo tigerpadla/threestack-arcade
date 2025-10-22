6bc5b91e39bad0eb0d9ef7f67a74787f39e5617c
ColeiosCode <jdburkecole@gmail.com>
2025-10-22 11:43:43 +0100

    Play board and background image set up.

---
+++ assets/js/snake.js
@@ -0,0 +1,8 @@
+const playBoard = document.querySelector(".play-board");
+
+let foodX = 13,
+    foodY = 10;
+
+const initGame = () => {
+    let htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
+};
